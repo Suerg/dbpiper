0e2e2a5a877120dc0ae07ef4b9345aa90cf949d1
unknown <dp0613@USSC1HXPSY1LP.hologic.corp>
2015-01-30 17:20:17 -0800

    Added sandbox.

---
+++ app/directives/sandbox.directive.js
@@ -0,0 +1,12 @@
+/**
+ * Created by dp0613 on 1/30/2015.
+ */
+angular.module('dbpiper').directive('sandbox', ['$document', function ($document) {
+    return {
+        restrict: 'AE',
+        link: function ($scope, element, attrs) {
+            element.css('width', attrs.width);
+            element.css('height', attrs.height);
+        }
+    }
+}]);
