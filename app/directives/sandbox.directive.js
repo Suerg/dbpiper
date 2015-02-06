7cafad1cdba03d5366eb196180815641956baf94
unknown <dp0613@USSC1HXPSY1LP.hologic.corp>
2015-02-06 12:34:31 -0800

    Adding stubs for mouse resizable sandbox code.

--- app/directives/sandbox.directive.js
+++ app/directives/sandbox.directive.js
@@ -10,9 +10,18 @@ angular.module('dbpiper').directive('sandbox', ['$document', function ($document
             };
         },
         link: function ($scope, element, attrs) {
+            var dragDelta = 20;
             element.css('width', attrs.width);
             element.css('height', attrs.height);
-
+            element.on('mousemove', function(event) {
+                var leftDelta = event.clientX - element[0].offsetLeft;
+                var rightDelta = (element[0].offsetLeft + element[0].offsetWidth) - event.clientX;
+                if(leftDelta <= dragDelta || rightDelta <= dragDelta) {
+                    element.addClass('resizeHorizontal');
+                } else {
+                    element.removeClass('resizeHorizontal');
+                }
+            });
         }
     }
 }]);
